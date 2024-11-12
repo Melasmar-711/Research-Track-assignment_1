8b673cbba9ec1712abc0c86e62ba8242943982de
Melasmar-711 <mahmoudelasmar2000@outlook.com>
2024-11-12 23:56:05 +0100

    configured the structure of my package and started to implement a class that represents the turtles Work in progess

---
+++ src/Distance.cpp
@@ -0,0 +1,33 @@
+/*
+
+A node that checks the relative distance between turtle1 and turtle2 and:
+- publish on a topic the distance (you can use a std_msgs/Float32 for
+that)
+- stops the moving turtle if the two turtles are “too close” (you may
+set a threshold to monitor that)
+- stops the moving turtle if the position is too close to the boundaries
+
+*/
+
+
+
+
+#include<ros/ros.h>
+
+
+
+
+
+
+int main(int argc ,char **argv)
+
+{
+
+
+
+
+
+
+
+
+}
---
+++ src/UI.cpp
@@ -0,0 +1,70 @@
+/*
+
+This node 
+
+-Spawns a new turtle in the environment: turtle2
+
+-Implements a simple textual interface to retrieve the user command
+(i.e., you can use cin (c++) or input (python). The user should be able to
+select the robot they want to control (turtle1 or turtle2), and the velocity
+of the robot.
+
+-The command should be sent for 1 second, and then the robot should
+stop, and the user should be able again to insert the command.
+
+*/
+
+
+
+
+
+class Turtle{
+
+public:
+string turtle_name;
+int x;
+int y;
+int theta;
+
+Turtle(string name ,int x_pos ,int y_pos, int theta_){
+
+turt_name=name;
+x=x_pos;
+y=y_pos;
+theta=theta_;
+
+}
+
+
+
+
+
+
+}
+int main(int argc , char **argv)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
