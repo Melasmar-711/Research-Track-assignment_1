95c5bae748f0a3e5ca97b38cac46e3c9eb32d66d
Melasmar-711 <mahmoudelasmar2000@outlook.com>
2024-11-15 02:44:33 +0100

    implemented a turtle class that will create a subscriber for the pose of each turtle depending on the number of turtles available in the simlation. which is an info that is provided by the UI_node and updated whenever a turtle is added or killed

--- src/Distance.cpp
+++ src/Distance.cpp
@@ -12,7 +12,63 @@ set a threshold to monitor that)
 
 
 
-#include<ros/ros.h>
+#include <string>
+#include "ros/ros.h"
+#include "turtlesim/Kill.h"
+#include "turtlesim/Spawn.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/Pose.h"
+
+
+
+
+class Turtle{
+
+public:
+
+
+
+
+std::string turtle_name;
+float x;
+float y;
+float theta;
+ros::NodeHandle n;
+
+void kill();
+void move_or_not_turtle(float V_x ,float Theta_dot,int timer);
+void get_pose_callback(const turtlesim::Pose::ConstPtr& msg);
+
+
+Turtle(ros::NodeHandle &n_,std::string name ){
+turtle_name=name;
+n=n_;
+std::string topic="/"+turtle_name+"pose";
+ros::Subscriber pose_subscriber=n.subscribe(topic,1000,&Turtle::get_pose_callback,this);
+}
+private:
+geometry_msgs::Twist turtle_cmd_vel ;
+};
+
+//-----------------------------------------------------------------------------------------//
+// methods of the Class Turtle 
+
+void Turtle::get_pose_callback(const turtlesim::Pose::ConstPtr& msg){
+
+x = msg->x;
+y = msg->y;
+theta = msg->theta;
+
+}
+
+
+/*this will move the turle for any amount of time the user chooses */
+void Turtle::move_or_not_turtle(float V_x ,float Theta_dot,int timer){
+    
+}
+
+
+
 
 
 
@@ -22,6 +78,11 @@ set a threshold to monitor that)
 int main(int argc ,char **argv)
 
 {
+
+
+    ros::init(argc,argv,"tutrtlesim_excercise_1");
+
+    ros::NodeHandle nh;
 
 
 
