a0facc2595c6c400a11170bec40cb899e50dfbe9
Melasmar-711 <Mahmoudelasmar2000@outlook.com>
2025-01-13 11:04:33 +0100

    added a publisher that publisher fake obstacles

---
+++ scripts/pub_obstacles.py
@@ -0,0 +1,23 @@
+import rospy
+from std_msgs.msg import Float32MultiArray
+import numpy as np
+
+
+# write a publisher that publises the obstacles in the world as a float32multiarrar of size 16 
+
+def pub_obstacles():   
+    pub = rospy.Publisher('obstacles', Float32MultiArray, queue_size=10)
+    rospy.init_node('obstacle_publisher', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        obstacles = Float32MultiArray()
+        obstacles.data = np.random.rand(16)  # put a range for values
+        pub.publish(obstacles)    
+
+
+
+if __name__ == '__main__':
+    try:
+        pub_obstacles()
+    except rospy.ROSInterruptException:
+        pass
